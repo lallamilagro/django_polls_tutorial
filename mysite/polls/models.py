252107e7678ca3c87e7bb0d8d7e79b7f8dcacb89
lallamilagro <for.lenochka@gmail.com>
2017-09-13 12:27:05 +1000

    Tests with fixing the bug

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,6 +4,9 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
 
+def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -20,3 +23,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self): 
         return self.choice_text
+       
